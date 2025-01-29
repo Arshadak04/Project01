22bc255d2a3d3d41abafec544f2056745a5c40aa
Arshad <arshadak04@gmail.com>
2025-01-29 16:11:47 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+ 
+// This is the how to react element created but we did not like this ,
+//  we create react elemnt through the jsx which is easy
 
-    // const heading=React.createElement("h1",{id:"child"},"Hello World");
-//  <div>
-//     <h1>
-//         Hello Its Arshad Qureshi Inside a Heading
-//     </h1>
-//  </div>
-    const parent=React.createElement(
-        "div",{id:"parent"},
+
+    // const parent=React.createElement(
+    //     "div",{id:"parent"},
     
-        [React.createElement(
-            "h1",{id:"child1"},"Hello Its Arshad Qureshi Inside a Heading H1",
+    //     [React.createElement(
+    //         "h1",{id:"child1"},"Hello Its Arshad Qureshi Inside a Heading H1",
                 
-            ),React.createElement(
-                "h2",{id:"child2"},"Hello Its Arshad Qureshi Inside a Heading H2",
+    //         ),React.createElement(
+    //             "h2",{id:"child2"},"Hello Its Arshad Qureshi Inside a Heading H2",
                     
-                )]
+    //             )]
         
-    ) 
+    // ) 
 
-    
+    const jsxheading=(<div>
+        <h1 id="child01">Heading 1 js</h1>
+        <h2 id="child02">Heading 2 js</h2>
+    </div>)
 
-        // [React.createElement(
-        //     "h1",{id:"child1"},"Hello Its Arshad Qureshi Inside a Heading H1",
-                
-        //     ),React.createElement(
-        //         "h2",{id:"child2"},"Hello Its Arshad Qureshi Inside a Heading H2",
-                    
-        //         )]
     
     const root=ReactDOM.createRoot(document.getElementById("root"))
-    root.render(parent);
+    root.render(jsxheading);
 
     
